dca8a662a875c0ea5644ed8d6dcb48acef593f98
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-22 23:05:57 +0300

    image to pdf start

---
+++ image2PDF.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+class ImagetoPDFConverter:
+    def __init__(self, root):
+        self.root = root
+        self.image_paths = []
+        self.output_pdf_name = tk.StringVar()
+        self.selected_image_listbox = tk.Listbox(root, selectmode = tk.MULTIPLE)
+
+        def initialize_ui(self):
+            title_label = tk.Label(self.root, text = "Image to PDF Converter", font = ("Arial", 20, "bold"))
+            title_label.pack(pady = 20)
