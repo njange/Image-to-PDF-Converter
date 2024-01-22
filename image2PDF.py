c18944fd7ba4715506d386ecab5d2fc0a13e0e2b
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-01-22 23:32:50 +0300

    image to pdf start

--- image2PDF.py
+++ image2PDF.py
@@ -8,17 +8,20 @@ class ImagetoPDFConverter:
         self.image_paths = []
         self.output_pdf_name = tk.StringVar()
         self.selected_image_listbox = tk.Listbox(root, selectmode = tk.MULTIPLE)
+        self.initialize_ui()
 
-        def initialize_ui(self):
-            title_label = tk.Label(self.root, text = "Image to PDF Converter", font = ("Arial", 20, "bold"))
-            title_label.pack(pady = 10)
+    def initialize_ui(self):
+        title_label = tk.Label(self.root, text = "Image to PDF Converter", font = ("Arial", 20, "bold"))
+        title_label.pack(pady = 10)
 
 def main():
-    root = tk.TK()
+    root = tk.Tk()
     root.title("Image to PDF Converter")
     root.geometry("400x600")
+    app = ImagetoPDFConverter(root)
     root.mainloop()
 
-if __name__ ==
+if __name__ == "__main__":
+    main()
 
 
